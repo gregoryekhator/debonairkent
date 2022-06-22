92619aead1743cfb0136549ffb69717476b5ddaf
Abbasumaru <38254329+Abbasumaru@users.noreply.github.com>
2022-06-22 04:56:32 +0100

    first commit

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 // The current module version (Date: YYYYMMDDXX).
-$plugin->version   = 2022062103;
+$plugin->version   = 2022062104;
 
 
 // Version maturity level.
